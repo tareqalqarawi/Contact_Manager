7386ed464c082c27b166fed66f77b352a30d5918
Tareq <103073154+tareqalqarawi@users.noreply.github.com>
2023-11-01 00:58:52 +0300

    Create massed.java

---
+++ massed.java
@@ -0,0 +1,10 @@
+
+public class HelloMassed {
+
+  public static void main(String[] args){
+    
+System.out.printf("massad is angry, because he wants to shg kfupm");
+System.out.print("tareq is happy, as he is writing java code");
+System.out.printf("mohammed is arguing");
+  
+}
